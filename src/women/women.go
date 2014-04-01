877ec44bc2b840d6a608af200be02f75ef9c3433
DevinDong <devin@DongDevintekiMacBook-Air.local>
2014-04-01 22:02:37 +0800

    add all

--- src/women/women.go
+++ src/women/women.go
@@ -11,18 +11,10 @@ type Head struct {
 	Ear   string
 }
 
-type Pussy struct {
-	Color string
-	Size  int
-	Shape string
-}
-
 type Body struct {
 	Head   Head
 	Breast string
 	Hands  string
-	Ashole string
-	Pussy  Pussy
 	Feet   string
 }
 
@@ -30,14 +22,4 @@ type Women struct {
 	Name string
 	Age  int
 	Body Body
-}
-
-func (w *Women) MakeLove() {
-	fmt.Println("I am ", w.Age, " now and my name is", w.Name)
-	fmt.Println("er...")
-}
-
-func main() {
-	women := Women{Name: "Nancy", Age: 18}
-	women.MakeLove()
 }
